8254bf0e0eaab76aa76a47230be5cb2cc33a9a3c
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-10-16 12:11:35 +0100

    Completion of L1, E2
    Completes shapes task which was part of Lab 1 exercise 2

---
+++ Lab 1/Lab 1 Submission/Lab 1 Code/src/shapes/Rectangle.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class Rectangle extends Shape {
+    protected int    centerX;
+    protected int centerY;
+    protected int length;
+    protected int width;
+
+    public Rectangle(int centerX, int centerY, int length, int width) {
+        this.name = "circle";
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.length = length;
+        this.width = width;
+    }
+
+    public void draw() {
+        System.out.println("This is a rectangle with center(" + centerX + "," + centerY + ")and size of: length " + length + " & width " + width);
+    }
+}
---
+++ Lab 1/Lab 1 Submission/Lab 1 Code/src/shapes/RedRectangle.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class RedRectangle extends Rectangle{
+    public RedRectangle(int centerX, int centerY, int length, int width) {
+        super(centerX, centerY, length, width);
+        this.color = "red";
+    }
+}
