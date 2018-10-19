593e37e3d079dad5cadb6ea3066e8eb045014efa
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-10-19 16:29:40 +0100

    Extra Credit for L1, E2

--- Lab 1/Lab 1 Submission/Lab 1 Code/src/Exercise2/Circle.java
+++ Lab 1/Lab 1 Submission/Lab 1 Code/src/Exercise2/Circle.java
@@ -1,18 +1,23 @@
 package Exercise2;
 
 public class Circle extends Shape {
+    // Def attributes for Circle objects
     protected int centerX;
     protected int centerY;
     protected int radius;
+    protected double area;
+
 
     public Circle(int centerX, int centerY, int radius) {
+        //Constructor for Circle objects
         this.name = "circle";
         this.centerX = centerX;
         this.centerY = centerY;
         this.radius = radius;
+        this.area = Math.PI * Math.pow(radius, 2);   //
     }
 
     public void draw() {
-        System.out.println("This is a circle with center(" + centerX + "," + centerY + ")and radius" + radius);
+        System.out.println("\n This is a circle with center(" + centerX + ", " + centerY + ") and radius" + radius + ".\nAlso the circle has an area of: " + String.format("%.1f",area));
     }
 }
