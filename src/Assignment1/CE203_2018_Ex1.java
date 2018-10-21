4cd412331036965cff1823fa00737c7cbf78cc90
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-10-21 01:39:35 +0100

    innit Validation Feature
    Addition of validation of the colour field inputs

--- src/Assignment1/CE203_2018_Ex1.java
+++ src/Assignment1/CE203_2018_Ex1.java
@@ -21,6 +21,7 @@ public class CE203_2018_Ex1 {
         JButton resetButton = new JButton("Reset");
         resetPanel.add(resetButton);
         resetButton.addActionListener(new ActionListener() {
+            //Action for when reset button pressed
             @Override
             public void actionPerformed(ActionEvent e) {
                 text.setForeground(Color.BLACK);
@@ -30,15 +31,21 @@ public class CE203_2018_Ex1 {
         //Colour setup here
         JPanel rgbPanel = new JPanel();
         rgbPanel.setLayout(new GridLayout(1, 4));
-        JTextField red = new JTextField(3);
-        JTextField green = new JTextField(3);
-        JTextField blue = new JTextField(3);
+        inputValidation red = new inputValidation();
+        inputValidation green = new inputValidation();
+        inputValidation blue = new inputValidation();
         JButton setButton = new JButton("Set");
         rgbPanel.add(red);
         rgbPanel.add(green);
         rgbPanel.add(blue);
         rgbPanel.add(setButton);
-
+        setButton.addActionListener(new ActionListener() {
+            // Action for when Set button is pressed
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                text.setForeground(new Color(red.validateColour(), green.validateColour(), blue.validateColour()));
+            }
+        });
         mainFrame.add(resetPanel);
         mainFrame.add(textPanel);
         mainFrame.add(rgbPanel);
---
+++ src/Assignment1/inputValidation.java
@@ -0,0 +1,25 @@
+package Assignment1;
+
+import javax.swing.*;
+
+public class inputValidation extends JTextField {
+    int value;
+    public int validateColour(){
+        try {
+            value = Integer.parseInt(getText());
+            if (value < 0) {
+                value = 200;
+                setText("200");
+            } else if (value > 255){
+                value = 255;
+                setText("255");
+            }
+            return value;
+        }
+        catch (Exception e) {
+        // value received is not an integer
+            setText("");
+        }
+        return value;
+    }
+}
