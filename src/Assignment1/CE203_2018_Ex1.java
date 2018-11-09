3675727d1e8202ff940736378b74073710a2d01c
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2018-11-09 12:40:04 +0000

    Ex1 Stuff Done

--- src/Assignment1/CE203_2018_Ex1.java
+++ src/Assignment1/CE203_2018_Ex1.java
@@ -4,13 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 //TODO ensure exceptions are handled by program
 
 
 
 public class CE203_2018_Ex1 {
-    public CE203_2018_Ex1() {
+    public static void main(String[] args) {
         JFrame mainFrame = new JFrame("CE203 Assignment 1");
         mainFrame.setSize(750, 300);
         mainFrame.setLayout(new BorderLayout());
@@ -66,8 +68,40 @@ public class CE203_2018_Ex1 {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
     }
+}
 
-    public static void main(String[] args) {
-        new CE203_2018_Ex1();
+class inputValidation extends JTextField {
+    public inputValidation(String t) {
+        super(t);
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                setText("");   //Clear colour label (R,G,B) from TextField on click
+            }
+        });
+    }
+
+    int value;
+
+    public int validateColour(JLabel label) {
+        if (getText().length() > 7) {
+            setText("255");
+        }    // Error handling to deal with strings that are too large to be integers!  Thus set to be 255.
+        try {
+            value = Integer.parseInt(getText());
+            if (value < 0) {
+                value = 200;
+                setText("200");
+            } else if (value > 255) {
+                value = 255;
+                setText("255");
+            }
+        } catch (Exception e) {
+            // Value received from this field is not an integer
+            label.setText("Invalid input in colour field(s)! Please try again below!");
+            label.setFont(new Font("Monaco", Font.BOLD | Font.ITALIC, 20));
+            setText("");
+            label.setForeground(Color.RED);  //TODO Actually set it as RED :/
+        }
+        return value;
     }
 }
