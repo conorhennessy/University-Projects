7af8acf65fbe0498659a1f0d67db7dfe7c326717
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-11-13 12:20:46 +0000

    Ex2 Sorted :anchor: :banana:
    loads of stuff done and shoutout to @alhockly for saving hours of work from destruction

--- src/Assignment1/CE203_2018_Ex2.java
+++ src/Assignment1/CE203_2018_Ex2.java
@@ -17,26 +17,30 @@ public class CE203_2018_Ex2 {
         mainFrame.setLayout(new BorderLayout());
 
         //Initialise LinkedList and set all that up
-        LinkedList<String> wordList = new LinkedList<String>();
+        LinkedList<String> wordList = new LinkedList<>();
 
         //Input text field setup
         JPanel inputPanel = new JPanel();
         inValidation inField = new inValidation();
         //TODO JTextField to be extended like in EX 1 to ensure only contain letters, numbers and hyphens.  Input Must begin with letter
         //TODO ensure appropriate error displayed for incorrect inputs
-        inField.setText("INPUT");
         inField.setColumns(50);
         inputPanel.add(inField);
 
 
         //System text output setup
+        JPanel innerPanel = new JPanel();
         JPanel textPanel = new JPanel();
         JLabel textLabel = new JLabel("Welcome to the word list store!");
         textLabel.setFont(new Font("Monaco", Font.BOLD, 25));
-        textPanel.add(textLabel);
+        textPanel.setLayout(new GridBagLayout());  // Inorder to get the text positioned center to the frame - both vertically and horizontally
+        textPanel.add(innerPanel);
+        innerPanel.add(textLabel);
+
 
 
         //Button Setup
+        // Many event handlers with one for each button as each is quite different and to show the coe more simply
         //TODO get each anonymous action listener to do stuff
         //TODO Ensure appropriate message and output is displayed in textLabel for each button press
         JPanel buttonPanel = new JPanel();
@@ -54,24 +58,44 @@ public class CE203_2018_Ex2 {
         wordsEndingSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {   // Action for when 'Add Word' button is pressed
-                textLabel.setText("Word Search");
-                if (inField.validateInput(textLabel)) {
-                    //TODO display all words in list that end with a specific letter, case-insensitive
+                if (inField.getText().length() != 1) {
+                    textLabel.setText("More than one character inputted, please try again...");
+                    textLabel.setForeground(Color.RED);  //TODO fix colouring of this to go back to black
+                }
+                else if (inField.validateInput(textLabel)) {
+                    String searchLetter = inField.getText().toLowerCase();
+                    LinkedList<String> tempList = new LinkedList<String>();
+                    for( String word : wordList){
+                        if (word.charAt(word.length() - 1) == searchLetter.charAt(0)){ tempList.add(word); }
+                    }
+                    if (tempList.size() == 0) {
+                        textLabel.setText("No words were found ending in \"" + searchLetter + "\"!");
+                    }
+                    else {
+                        textLabel.setText(tempList.size() + " Words found ending in \"" + searchLetter + "\" : " + Arrays.toString(tempList.toArray()));
+                    }
                 }
             }
         });
-        JButton wordOccurrences = new JButton("Find occurrences");
+        JButton wordOccurrences = new JButton("Find occurrences");  //TODO fix positions of where found
         wordOccurrences.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {   // Action for when 'Add Word' button is pressed
                 if (inField.validateInput(textLabel)) {
-                    int count = 0;
-                    //for (word:wordList){
-                      //  if (inField.getText().equals(word))
-                        //count++;
-                    //}  //TODO this mess of a for loop needs fixing  see to Unit 4: Weeks 5 to 6 lecture notes
-                    textLabel.setText("\"" + inField.getText() + "\" occurs " + count + " times in the list.");
-                    //TODO search list for word and display occurrences & positions in list where it is found.  If input empty - display total number of words in list and a nice message
+                    if (inField.getText().length() == 0) {
+                        textLabel.setText("There are currently " + wordList.size() + " words in the list.");
+                    }
+                    else {
+                        int count = 0;
+                        ArrayList<Integer> posList = new ArrayList<>();
+                        for (String word : wordList) {
+                            if (inField.getText().equals(word)) {
+                                count++;
+                                posList.add(wordList.indexOf(word));
+                            }
+                        }
+                        textLabel.setText("\"" + inField.getText() + "\" occurs " + count + " time(s) in the list. At position(s): " + Arrays.toString(posList.toArray()));
+                    }
                 }
             }
         });
@@ -79,7 +103,7 @@ public class CE203_2018_Ex2 {
         removeLast.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {   // Action for when 'Add Word' button is pressed
-                textLabel.setText("The last occurence of \"" + inField.getText() + "\" Has been removed");
+                textLabel.setText("The last occurrence of \"" + inField.getText() + "\" Has been removed");
                 if (inField.validateInput(textLabel)) {
                     wordList.removeLastOccurrence(inField.getText());
 
@@ -90,9 +114,9 @@ public class CE203_2018_Ex2 {
         removeAll.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {   // Action for when 'Add Word' button is pressed
-                textLabel.setText("Remove all occurrences");
+                textLabel.setText("All occurrences of " + inField.getText() + " have been removed.");
                 if (inField.validateInput(textLabel)) {
-                    //TODO remove all occurrences of input from the list
+                    wordList.removeAll(Arrays.asList(inField.getText()));
                 }
             }
         });
@@ -100,9 +124,9 @@ public class CE203_2018_Ex2 {
         clearList.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {   // Action for when 'Add Word' button is pressed
-                textLabel.setText("The list has now been cleared");
+                textLabel.setText("The list has now been cleared!");
                 if (inField.validateInput(textLabel)) {
-                    //TODO simply clear the list
+                    wordList.clear();
                 }
             }
         });
@@ -125,16 +149,22 @@ public class CE203_2018_Ex2 {
 }
 
 class inValidation extends JTextField {
-    public boolean validateInput(JLabel label) {
+    public inValidation() {
         this.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 setText("");   //Clear INPUT label from TextField on click
             }
         });
+    }
 
+    public boolean validateInput(JLabel label) {
         try{
-            // TODO Error handling for ensuring words are the structure as required
-            return true;
+            // TODO Handling for ensuring words are the structure as required. May only letters, numbers, and hyphens (-) and must begin with a letter
+            String regEx = "^[a-zA-Z][a-zA-Z1-9-]*";
+            if (this.getText().matches(regEx)) {
+                return true;
+            }
+            else return false;
         }
         catch (Exception e){
             // Value received from this field is raising an error
@@ -146,3 +176,4 @@ class inValidation extends JTextField {
         }
     }
 }
+
