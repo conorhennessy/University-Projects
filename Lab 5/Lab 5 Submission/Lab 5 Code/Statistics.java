4cdbbf13d4870e25c81c5ab71c949e23772ed360
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-11-23 19:17:18 +0000

    Init commit of lab 5

---
+++ Lab 5/Lab 5 Submission/Lab 5 Code/Statistics.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Statistics {
+    Iterator iter =
+    static int maximum(ArrayList<Integer> list){
+        Iterator iter = list.iterator();
+        Integer max =
+        while (iter.hasNext()){
+            if ()
+
+        }
+        return max;
+    }
+    static int minimum(ArrayList<Integer> list){
+        return Collections.min(list);
+    }
+    static double adverage(ArrayList<Integer> list){
+        return Collections;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
