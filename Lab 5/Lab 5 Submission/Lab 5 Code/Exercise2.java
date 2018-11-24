c390afa54ca94f92c9433644015f8a16238134b3
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2018-11-24 00:17:41 +0000

    Loads of Lab 5 sorted

--- Lab 5/Lab 5 Submission/Lab 5 Code/Exercise2.java
+++ Lab 5/Lab 5 Submission/Lab 5 Code/Exercise2.java
@@ -1,18 +1,44 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise2 {
-    static List<Integer> sharedItems(List<Integer> list1, List<Integer> list2){
-        List<Integer> sharedList = null;
-        int listSize = list1.size() < list2.size() ? list1.size() : list2.size();
-        for (int i = 0; i < listSize; i++){
-            if (list1.get(i) == list2.get(i)){
-                sharedList.add(list1.get(i));
+    // SharedItems method which compares two lists in order to find terms which are found in both lists.
+    static List<Integer> sharedItems(List<Integer> list1, List<Integer> list2) {
+        System.out.println("List 1: " + list1 + "\nList 2: " + list2);
+        List<Integer> sharedList = new ArrayList<>();
+        try {
+            for (Integer x : list1) {
+                for (Integer y : list2){
+                    if (x == y) {
+                        sharedList.add(x);
+                    }
+                }
             }
+            System.out.println("\nThe shared item(s) between these two lists are... " + sharedList);
         }
+        catch (Exception e){
+            System.out.println("Unable to find any matches between the two lists...");
+        }
+
         return sharedList;
     }
 
     public static void main(String[] args) {
-    
+        List<Integer> firstList = new ArrayList<>();
+        //firstList.add(5);
+        firstList.add(3);
+        //firstList.add(7);
+        firstList.add(2);
+        //firstList.add(4);
+
+        List<Integer> secondList = new ArrayList<>();
+        secondList.add(4);
+        //secondList.add(7);
+        secondList.add(6);
+        //secondList.add(5);
+        secondList.add(8);
+
+        sharedItems(firstList, secondList);
+
     }
 }
---
+++ Lab 5/Lab 5 Submission/Lab 5 Code/Exercise3.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Exercise3 {
+    List
+
+    public static void main(String[] args) {
+
+    }
+}
