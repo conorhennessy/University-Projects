6f40b6d4e199476ed676700c46220c8dd1702976
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2018-11-24 14:37:35 +0000

    Lab 5 Complete

--- Lab 5/Lab 5 Submission/Lab 5 Code/Exercise2.java
+++ Lab 5/Lab 5 Submission/Lab 5 Code/Exercise2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise2 {
-    // SharedItems method which compares two lists in order to find terms which are found in both lists.
+    // SharedItems method which compares two lists in order to find terms which are found in both lists. Returns sharedList of all terms matching
     static List<Integer> sharedItems(List<Integer> list1, List<Integer> list2) {
         System.out.println("List 1: " + list1 + "\nList 2: " + list2);
         List<Integer> sharedList = new ArrayList<>();
@@ -24,20 +24,22 @@ public class Exercise2 {
     }
 
     public static void main(String[] args) {
+        // Creation of two ArrayLists to be used for testing
         List<Integer> firstList = new ArrayList<>();
-        //firstList.add(5);
+        firstList.add(5);
         firstList.add(3);
-        //firstList.add(7);
+        firstList.add(7);
         firstList.add(2);
-        //firstList.add(4);
+        firstList.add(4);
 
         List<Integer> secondList = new ArrayList<>();
         secondList.add(4);
-        //secondList.add(7);
+        secondList.add(7);
         secondList.add(6);
-        //secondList.add(5);
+        secondList.add(5);
         secondList.add(8);
 
+        // Call of method to make comparison between lists
         sharedItems(firstList, secondList);
 
     }
