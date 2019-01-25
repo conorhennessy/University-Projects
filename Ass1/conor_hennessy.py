0cb5bd3ba11d975ab263689cff9d6dc332f5de69
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2019-01-25 01:16:33 +0000

    Attempt to appending line
    Not exactly working as it pust it at the start of the line rather than the end. Need to fix

--- Ass1/conor_hennessy.py
+++ Ass1/conor_hennessy.py
@@ -1,27 +1,34 @@
-from sfs import *
+from Ass1.sfs import *
+
 
 def conor_hennessy( filename ):
     # Open 'sfs.py' file in cd
     pyFile = 'sfs.py'
 
     if ( os.path.isfile( pyFile ) ):
-
         file = open( pyFile, 'r' )
         line_list = file.readlines()
-        sfs_compute_size( line_list )
         file.close()
-
+        file = open( pyFile, 'w' )
     else:
-        print( "File " + filename + " does not exist." )
+        print( "File " + pyFile + " does not exist." )
 
     # TODO goto line 52 and add '; print ( "virus" )' to the line
+    # Done by reading the file storing all lines in a list, rewriting that line and then saving back to file
+    count = 0
+    for line in line_list:
+        count += 1
+        if count == 52:
+            line = line + '; print ( "virus" )'
+        # Now save that line back to file overriding it.
+        file.write(line)
 
+    file.close()
 
     # TODO save modified file under its original name
 
     # Open and run sfs.py file
-    sys( filename )
-
+    sfs( filename )
 
 
 if __name__ == '__main__':
