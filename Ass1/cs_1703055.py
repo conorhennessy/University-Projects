51dc74f3aa5f9976e653b00fb0a8993512929b0f
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2019-01-28 11:40:33 +0000

    Tiny tweaks
    Changed function name, tidied up comments and removed call to run file as this is not neccesary - was outlined to me by GLA

--- Ass1/cs_1703055.py
+++ Ass1/cs_1703055.py
@@ -1,13 +1,10 @@
-from Ass1.sfs import *
+from sfs import *
 
-
-def conor_hennessy( filename ):
-    pyFile = 'sfs.py'
-
-
+def cs_1703055():
     ## Open 'sfs.py' file in cd
     # Done by opening the file for reading, storing lines into a list,
     # close the file and then open the file for writing - ready for next step.
+    pyFile = 'sfs.py'
     if ( os.path.isfile( pyFile ) ):
         file = open( pyFile, 'r' )
         line_list = file.readlines()
@@ -17,8 +14,8 @@ def conor_hennessy( filename ):
         print( "File " + pyFile + " does not exist." )
 
     ## Goto line 52 and add '; print ( "virus" )' to the line
-    # Done by reading the file storing all lines in a list,
-    # rewriting the 52nd line, write rest of lines and then saving back to file
+    # Done by already reading the file, storing all lines in a list,
+    # rewriting the 52nd line, write rest of lines to file and then closing the file.
     count = 0
     for line in line_list:
         count += 1
@@ -28,9 +25,6 @@ def conor_hennessy( filename ):
         file.write(line)
     file.close()
 
-    # Open and run sfs.py file
-    sfs( filename )
-
 
 if __name__ == '__main__':
-    conor_hennessy( "file.txt" )
+    cs_1703055()
