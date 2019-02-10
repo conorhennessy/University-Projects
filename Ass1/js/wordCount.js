8dd1086d6ed8ecbc9d147a1c4d34a9f73b391901
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2019-02-10 17:13:09 +0000

    Exercise 2 complete
    All done but it has a big fat open and then count button which must each be clicked...

--- Ass1/js/wordCount.js
+++ Ass1/js/wordCount.js
@@ -40,7 +40,48 @@ function wordCount() {
         }
     }
 
-    console.log(wordFrequencies);
+
+    //Now to output this as a table
+    var parent = document.getElementById("wordCount");
+
+    //If table already exists remove it to allow for creating a new one.
+    if (document.contains(document.getElementById("countTable"))) {
+        document.getElementById("countTable").remove();
+    }
+
+    var table = document.createElement("table");
+    table.id = "countTable";
+    parent.appendChild(table);
+
+    var tHead = document.createElement("thead");
+    var hRow = document.createElement("tr");
+    var head1 = document.createElement("th");
+    head1.innerText = "Word";
+    var head2 = document.createElement("th");
+    head2.innerText = "Count";
+
+    tHead.appendChild(hRow);
+    hRow.appendChild(head1);
+    hRow.appendChild(head2);
+    table.appendChild(tHead);
+
+
+    var tBody = document.createElement("tbody");
+
+    for (var word in wordFrequencies){
+        var row = document.createElement("tr");
+        var c1Data = document.createElement("td");
+        var c2Data = document.createElement("td");
+
+        c1Data.innerText = word;
+        c2Data.innerText = wordFrequencies[word];
+
+        row.appendChild(c1Data);
+        row.appendChild(c2Data);
+        tBody.appendChild(row);
+    }
+
+    table.appendChild(tBody);
 
 }
 
@@ -60,56 +101,6 @@ function retrieve(node) {
 
 
 /*
-    var url = document.getElementById("url").value;
 
-    //The drawing and creation of the table
-    var parent = document.getElementById("wordCount");
-
-    //If table already exists remove it to allow for creating a new one.
-    if (document.contains(document.getElementById("countTable"))) {
-        document.getElementById("countTable").remove();
-    }
-
-    var table = document.createElement("table");
-    table.id = "countTable";
-    parent.appendChild(table);
-
-    var head1Title = "Word";
-    var head2Title = "Count";
-
-
-    var tHead = document.createElement("thead");
-    var hRow = document.createElement("tr");
-    var head1 = document.createElement("th");
-    head1.innerText = head1Title;
-    var head2 = document.createElement("th");
-    head2.innerText = head2Title;
-
-    tHead.appendChild(hRow);
-    hRow.appendChild(head1);
-    hRow.appendChild(head2);
-    table.appendChild(tHead);
-
-
-
-    var tBody = document.createElement("tbody");
-
-    for (var i = 0 ; i < nodeCount ; i++){
-        var row = document.createElement("tr");
-        var c1Data = document.createElement("td");
-        var c2Data = document.createElement("td");
-
-        c1Data.innerText = "test";
-        c2Data.innerText = "test";
-
-        if (i % 2 == 1) row.setAttribute("class", "odd");
-        else row.setAttribute("class", "even");
-
-
-        row.appendChild(c1Data);
-        row.appendChild(c2Data);
-        tBody.appendChild(row);
-    }
-    table.appendChild(tBody);
 }
 */
