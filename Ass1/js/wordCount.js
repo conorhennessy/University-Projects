c5cbf5dd1e4ad787481c567767b22aada478824c
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2019-02-10 16:59:40 +0000

    Open and count words
    Now to just get it to output this stuff!

--- Ass1/js/wordCount.js
+++ Ass1/js/wordCount.js
@@ -1,8 +1,66 @@
+var w;
+var nodeCount;
+var arr = [];
+var temp = [];
+
+function openFile(url) {
+    w = window.open();
+    w.location = url;
+}
+
 function wordCount() {
+    nodeCount = 0;
+    retrieve(w.document.body);
+
+    //Cleaning up of returned arr to make it into a big long list of all the words
+    temp = arr;
+    arr = [];
+    for (var i = 0; i < temp.length; i++) {
+        var splitString = temp[i].trim().split(" ");
+        for (var j = 0; j < splitString.length; j++){
+            if (splitString != "") {
+                arr.push(splitString[j].toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""));
+            }
+        }
+    }
+
+    //Sort list into alphabetic order
+    arr.sort();
+
+    //Find number of occurrences of words in list and form this as a list
+    var wordFrequencies = {};
+    for (var num = 0; num < arr.length; num++) {
+        if (wordFrequencies.hasOwnProperty(arr[num])){
+            //If word is already in map increment
+            wordFrequencies[arr[num]]++;
+        }
+        else {
+            //As word not in map, add it wit initial value of 1
+            wordFrequencies[arr[num]] = 1;
+        }
+    }
+
+    console.log(wordFrequencies);
+
+}
+
+function retrieve(node) {
+    if (node.nodeType==Node.ELEMENT_NODE) {
+        for (var m = node.firstChild; m!=null; m = m.nextSibling) {
+            retrieve(m);
+        }
+    }
+    else if (node.nodeType==Node.TEXT_NODE) {
+        if (node.data.length > 1 && node.data != "") {
+            arr[nodeCount++] = node.data;
+        }
+    }
+}
+
+
+
+/*
     var url = document.getElementById("url").value;
-
-
-
 
     //The drawing and creation of the table
     var parent = document.getElementById("wordCount");
@@ -36,7 +94,7 @@ function wordCount() {
 
     var tBody = document.createElement("tbody");
 
-    for (var i = 0 ; i < counter ; i++){
+    for (var i = 0 ; i < nodeCount ; i++){
         var row = document.createElement("tr");
         var c1Data = document.createElement("td");
         var c2Data = document.createElement("td");
@@ -54,3 +112,4 @@ function wordCount() {
     }
     table.appendChild(tBody);
 }
+*/
