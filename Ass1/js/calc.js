796db1642d73149c3de7706f68422068c9aa0a7c
conorhennessy <9257022+conorhennessy@users.noreply.github.com>
2019-02-12 19:02:15 +0000

    Working calculator!
    Calculator now works now to tidy up code and make some CSS up for it!

--- Ass1/js/calc.js
+++ Ass1/js/calc.js
@@ -1,17 +1,62 @@
 var numberOutput;
+var storedOpr;
+var storedNum;
 
-function number(num){
+function number(num) {
+    console.log("NUM BUTTON PRESSED > num: " + num + "\n storedOpr: " + storedOpr + "\n storedNum: " + storedNum);
+
     numberOutput = document.getElementById("output");
-
-    if (numberOutput.textContent == "0"){
+    //if (numberOutput.textContent === "0" || (parseInt(numberOutput.textContent) !== storedNum && storedNum != null)){
+    if (numberOutput.textContent === "0" || (parseFloat(numberOutput.textContent) === storedNum)) {
+        console.log("Plan 1");
         numberOutput.textContent = num;
-    } else {
+    } else if (numberOutput.textContent !== "0") {
+        console.log("Plan 2");
         numberOutput.textContent = numberOutput.textContent + "" + num;
     }
-    console.log("number");
 }
 
-function clear(){
+function clearCalc(){
+    storedOpr = null;
+    storedNum = null;
     document.getElementById("output").textContent = "0";
-    console.log("clear");
+}
+
+function decimal() {
+    if (!numberOutput.textContent.includes(".") ){
+        numberOutput.textContent = numberOutput.textContent + ".";
+    }
+}
+
+function operator(opr) {
+    console.log("INITIAL VALUES > OPR: "+ opr + "\n storedOpr: "+ storedOpr + "\n storedNum: " + storedNum);
+
+    if (opr === storedOpr){
+        equal();
+        console.log("Plan A");
+    }
+    if (storedOpr == null || opr !== storedOpr) {
+        storedNum = parseFloat(numberOutput.textContent);
+        storedOpr = opr;
+        console.log("Plan B");
+    }
+    console.log("BUTTON PRESSED > OPR: "+ opr + "\n storedOpr: "+ storedOpr + "\n storedNum: " + storedNum);
+}
+
+function equal() {
+    console.log("evaluating");
+    if (storedOpr === '+'){
+        numberOutput.textContent = parseFloat(numberOutput.textContent) + parseFloat(storedNum);
+    }
+    if (storedOpr === '-'){
+        numberOutput.textContent = parseFloat(storedNum) - parseFloat(numberOutput.textContent);
+    }
+    if (storedOpr === '*'){
+        numberOutput.textContent = parseFloat(storedNum) * parseFloat(numberOutput.textContent);
+    }
+    if (storedOpr === '/'){
+        numberOutput.textContent = parseFloat(storedNum) / parseFloat(numberOutput.textContent);
+    }
+    storedNum = null;
+    storedOpr = null;
 }
