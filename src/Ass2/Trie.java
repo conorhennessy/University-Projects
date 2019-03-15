adeaad7fbcb78e135be003ce8d1fca9374d2b7d8
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2019-03-15 15:49:59 +0000

    Some work towards first task

--- src/Ass2/Trie.java
+++ src/Ass2/Trie.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,66 +15,74 @@ class Trie {
      */
     private TrieNode<Character> root;
 
-    public Trie() {
+    private Trie() {
         root = new TrieNode(null);
     }
 
 
-    public boolean addWord(String word){
+    private boolean addWord(String word) {
         word = word.toUpperCase();
-        if (!word.matches("[a-zA-Z]+")){
+        if (!word.matches("[A-Z]+")) {
             System.out.println("ERROR: String supplied contains non-letters!");
             return false;
         }
-        if (find(word)){
+        if (find(word)) {
+            // word already exists in trie, so will not be added and return false
             return false;
-        }
-        else {
+        } else {
             //Checks are complete, now try adding word to Trie
             TrieNode node = root;
 
-            for (int i = 0; i < word.length(); i++){
+            for (int i = 0; i < word.length(); i++) {
                 if (node.children == null) {
-                    //if tree is empty, if so just add it
+                    //if tree is empty, if so just add it and step into it
                     root.children.put(word.charAt(i), new TrieNode<>(word.charAt(i)));
-                } else if (node.children.containsKey(word.charAt(i))){
-                    // if children contains letter step into that node
-                } else {
-                    // else the children do not contain the letter so need to add letter to the children of this node
+                    node = (TrieNode) node.children.get(word.charAt(i));
+                } else if (!node.children.containsKey(word.charAt(i))) {
+                    // Children do not contain the letter so add letter to the children of this node and step into it
                     node.children.put(word.charAt(i), new TrieNode<>(word.charAt(i)));
-                }
-                node = (TrieNode) node.children.get(word.charAt(i));
-                if (i == word.length() - 1){
+                    node = (TrieNode) node.children.get(word.charAt(i));
+                } if (i == word.length() - 1){
                     // Word is complete, loop will be ending after this executes so set boolean flag to true
                     node.word = true;
-                }
-            }
-            return true;
-        }
-    }
-
-    public boolean find(String word){
-        TrieNode node = root;
-
-        for (int i = 0; i < word.length(); i++){
-            if (node.children.containsKey(word.charAt(i))){
-                if (i == word.length() - 1){
-                    // Letter is found but it's the last letter in word so we are done!
                     return true;
                 }
-                // Letter is found so step into that node
-                node = (TrieNode) node.children.get(word.charAt(i));
+            }
+        }
+        return false;
+    }
 
+    private boolean find(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.children.containsKey(word.charAt(i))) {
+                if (i == word.length() - 1) {
+                    return true;
+                }
+                node = (TrieNode) node.children.get(word.charAt(i));
             } else {
                 return false;
             }
         }
+        return false;
     }
 
-    public List<String> getWords(char c){
-        List<String> words = null;
+    public List<String> getWords(char c) {
+        List<String> words = new ArrayList<>();
 
+        if (!root.children.containsKey(c)) {
+            // First children of trie does not contain the letter supplied, so no words can exist
+            System.out.println("ERROR: Trie does not contain any words starting with\"" + c + "\"!");
+            return words;
+        }
+        if (!String.valueOf(c).matches("[a-zA-Z]+")) {
+            System.out.println("ERROR: String supplied contains non-letters!");
+            return words;
+        } else {
+            TrieNode node = root;
 
+            //TODO
+        }
 
         return words;
     }
@@ -81,14 +90,15 @@ class Trie {
 
     public static void main(String[] args) {
         Trie testTrie = new Trie();
-        testTrie.addWord("beast");
+        testTrie.addWord("pineapple");
         System.out.println(testTrie.root.children);
     }
 
 }
+
 class TrieNode<Character> {
     Character character;
-    Map<Character, TrieNode> children;
+    Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
     Boolean word;
 
     TrieNode(Character o) {
